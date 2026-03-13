f57cf04c839355f3e7cf807cc6228e29c5906151
Odonno <bottiau.david@laposte.net>
2026-03-13 18:18:20 +0100

    🔧 configure TanStack devtools

--- src/mainview/App.tsx
+++ src/mainview/App.tsx
@@ -1,5 +1,6 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { queryClient } from "./lib/query-client";
 import { routeTree } from "./routeTree.gen";
 
@@ -15,6 +16,7 @@ export default function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
+			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);
 }
